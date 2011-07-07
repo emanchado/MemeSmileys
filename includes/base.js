82fca4f9bde8eb4f419e25767e82e3607f81a0d5
Esteban Manchado Velázquez <estebanm@opera.com>
2011-07-07 12:51:49 +0200

    Loop properly

--- includes/base.js
+++ includes/base.js
@@ -6,7 +6,7 @@ function replaceSmileys(where) {
     }
     for (var ci = 0, l = classes.length; ci < l; ci++) {
         var nodes = where.getElementsByClassName(classes[ci]);
-        for (var i in nodes) {
+        for (var i = 0, len2 = nodes.length; i < len2; i += 1) {
             if (nodes[i].innerHTML) {
                 nodes[i].innerHTML = smileyParser.parseSmileys(nodes[i].innerHTML);
             }
